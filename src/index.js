5ad1802060e7e1f81ea4d2aef82103800ad58ccc
Luis Muñoz <lrmunoz@gmail.com>
2017-08-11 15:25:51 +0200

    Reorganize default places

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css'
 
 // eslint-disable-next-line react/no-render-return-value
 const app = ReactDOM.render(<App />, document.getElementById('root'))
-app.addPlace('Ahmedabad, India', 'IST', 5.5)
-app.addPlace('Córdoba, Spain', 'CEST', 2)
-app.addPlace('Michigan, USA', 'EDT', -5)
-app.addPlace('Ala./Minn., USA', 'CDT', -6)
 app.addPlace('California, USA', 'PDT', -7)
+app.addPlace('Alabama, USA', 'CDT', -6)
+app.addPlace('Michigan, USA', 'EDT', -5)
+app.addPlace('Córdoba, Spain', 'CEST', 2)
+app.addPlace('Ahmedabad, India', 'IST', 5.5)
