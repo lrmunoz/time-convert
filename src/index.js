b258c28e34c017f9625deae2dc93c54e8718e418
Luis Muñoz <lrmunoz@gmail.com>
2017-08-17 11:33:36 +0200

    Fix daylight timezones offset

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css'
 // eslint-disable-next-line react/no-render-return-value
 const app = ReactDOM.render(<App />, document.getElementById('root'))
 app.addPlace('California, USA', 'PDT', -7)
-app.addPlace('Alabama, USA', 'CDT', -6)
-app.addPlace('Michigan, USA', 'EDT', -5)
+app.addPlace('Alabama, USA', 'CDT', -5)
+app.addPlace('Michigan, USA', 'EDT', -4)
 app.addPlace('Córdoba, Spain', 'CEST', 2)
 app.addPlace('Ahmedabad, India', 'IST', 5.5)
