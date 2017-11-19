174e02c5e97d8cde1989033090aa6155ff90c763
Luis Muñoz <lrmunoz@gmail.com>
2017-11-19 15:30:16 +0100

    Replace initial places with encoded url

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import './index.css'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
 import theme from './toolbox/theme'
 import './toolbox/theme.css'
-import _ from 'lodash'
+import { decodePlacesUrl, encodePlacesUrl } from './UrlEncoder'
 
-let places = _([
-  ['California, USA', 'America/Los_Angeles'],
-  ['Alabama, USA', 'America/Chicago'],
-  ['Michigan, USA', 'America/Detroit'],
-  ['Córdoba, Spain', 'Europe/Madrid'],
-  ['Ahmedabad, India', 'Asia/Kolkata'],
-  ['American Samoa', 'Pacific/Pago_Pago'],
-  ['Kiritimati', 'Pacific/Kiritimati']
-]).map((cur) => _.zipObject(['placeName', 'ianaTimezone'], cur)).value()
-
-ReactDOM.render(<ThemeProvider theme={theme}><App places={places}/></ThemeProvider>, document.getElementById('root'))
+ReactDOM.render(<ThemeProvider theme={theme}><App places={decodePlacesUrl(window.location.hash.substring(1)) || []}/></ThemeProvider>, document.getElementById('root'))
